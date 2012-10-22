28916a3dffb1fb29d38a4192bd411f9696718715
Fabian Pedregosa <fabian@fseoane.net>
2012-10-22 20:25:06 +0200

    more

--- group_lasso.py
+++ group_lasso.py
@@ -1,35 +1,10 @@
 #
 # Author: Fabian Pedregosa <fabian@fseoane.net>
 # License: BSD
-import random
+
 import numpy as np
 from scipy import linalg
 
-
-## .. dual gap ..
-#max_inc = linalg.norm(w_old - w_new, np.inf)
-#if max_inc < rtol * np.amax(w_new):
-#    residual = np.dot(X, w_new) - y
-#    group_norm = alpha * np.sum([linalg.norm(w_new[g], 2)
-#                                 for g in group_labels])
-#    if H is not None:
-#        norm_Anu = [linalg.norm(np.dot(H[g], w_new) - Xy[g])\
-#                    for g in group_labels]
-#    else:
-#        # TODO: Uses H !!!!
-#        norm_Anu = [linalg.norm(np.dot(X[:, g].T, residual))\
-#                    for g in group_labels]
-#    if np.any(norm_Anu > alpha):
-#        nnu = residual * np.min(alpha / norm_Anu)
-#    else:
-#        nnu = residual
-#    primal_obj =  .5 * np.dot(residual, residual) + group_norm
-#    dual_obj   = -.5 * np.dot(nnu, nnu) - np.dot(nnu, y)
-#    dual_gap = primal_obj - dual_obj
-#    if verbose:
-#        print 'Relative error: %s' % (dual_gap / dual_obj)
-#    if np.abs(dual_gap / dual_obj) < rtol:
-#        break
 
 MAX_ITER = 1000
 
@@ -37,7 +12,7 @@ def soft_threshold(a, b):
     """accepts vectors"""
     return np.sign(a) * np.fmax(np.abs(a) - b, 0)
 
-#@profile
+
 def sparse_group_lasso(X, y, alpha, rho, groups, max_iter=MAX_ITER, rtol=1e-6,
                 verbose=False):
     """
@@ -91,24 +66,24 @@ def sparse_group_lasso(X, y, alpha, rho, groups, max_iter=MAX_ITER, rtol=1e-6,
     Xy = np.dot(X.T, y)
     K = np.dot(X.T, X)
     step_size = 1. / (linalg.norm(X, 2) ** 2)
+    _K = [K[g][:, g] for g in group_labels]
 
     for n_iter in range(max_iter):
         w_old = w_new.copy()
-        random.shuffle(group_labels)
-        for i, g in enumerate(group_labels):
-            w_tmp = w_new.copy()
-            w_tmp[g] = 0.
-            Kg = K[g]
-            X_residual = Xy[g] - np.dot(Kg, w_tmp)
-            residual = y - np.dot(X, w_tmp)
-            s = soft_threshold(X_residual, alpha * rho)
+        perm = np.random.permutation(len(group_labels))
+        X_residual = Xy - np.dot(K, w_new) # could be updated, but kernprof says it's peanuts
+        for i in perm:
+            g = group_labels[i]
+            Kgg = _K[i]
+            X_r_k = X_residual[g] + np.dot(Kgg, w_new[g])
+            s = soft_threshold(X_r_k, alpha * rho)
             # .. step 2 ..
             if np.linalg.norm(s) <= (1 - rho) * alpha:
                 w_new[g] = 0.
             else:
                 # .. step 3 ..
-                for _ in range(3 * g.size): # just a heuristic
-                    grad_l =  - (X_residual - np.dot(Kg[:, g], w_new[g]))
+                for _ in range(2 * g.size): # just a heuristic
+                    grad_l =  - (X_r_k - np.dot(Kgg, w_new[g]))
                     tmp = soft_threshold(w_new[g] - step_size * grad_l, step_size * rho * alpha)
                     tmp *= max(1 - step_size * (1 - rho) * alpha / np.linalg.norm(tmp), 0)
                     delta = linalg.norm(tmp - w_new[g])
@@ -117,14 +92,13 @@ def sparse_group_lasso(X, y, alpha, rho, groups, max_iter=MAX_ITER, rtol=1e-6,
                         break
 
                 assert np.isfinite(w_new[g]).all()
-
         if np.linalg.norm(w_new - w_old) / np.linalg.norm(w_new) < rtol:
             break
     return w_new
 
 
 
-def group_lasso_check_kkt(A, b, x, penalty, groups):
+def group_lasso_check_kkt(A, b, x, alpha, groups):
     """Auxiliary function.
     Check KKT conditions for the group lasso
 
@@ -132,15 +106,15 @@ def group_lasso_check_kkt(A, b, x, penalty, groups):
     """
     from scipy import linalg
     group_labels = [groups == i for i in np.unique(groups)]
-    penalty = penalty * A.shape[0]
+    alpha = alpha * A.shape[0]
     z = np.dot(A.T, np.dot(A, x) - b)
-    safety_net = .5 # sort of tolerance
+    safety_net = .1 # sort of tolerance
     for g in group_labels:
         if linalg.norm(x[g]) == 0:
-            if not linalg.norm(z[g]) < penalty + safety_net:
+            if not linalg.norm(z[g]) < alpha + safety_net:
                 return False
         else:
-            w = - penalty * x[g] / linalg.norm(x[g], 2)
+            w = - alpha * x[g] / linalg.norm(x[g], 2)
             if not np.allclose(z[g], w, safety_net):
                 return False
             return True
@@ -164,8 +138,8 @@ if __name__ == '__main__':
     coefs = sparse_group_lasso(X, y, alpha, 0., groups, verbose=True)
     print('KKT conditions verified:', group_lasso_check_kkt(X, y, coefs, alpha, groups))
 
-    X = np.random.randn(100, 1000)
-    y = np.random.randn(100)
+    X = np.random.randn(1000, 300)
+    y = np.random.randn(1000)
     groups = np.arange(X.shape[1]) // 100
     coefs = sparse_group_lasso(X, y, alpha, 0., groups, verbose=True)
     print('KKT conditions verified:', group_lasso_check_kkt(X, y, coefs, alpha, groups))
